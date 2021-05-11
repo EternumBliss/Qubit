b6f1db1269b2ba2a0ff8ca13103af40a7910a791
EternumBliss <amantol2702@gmail.com>
2021-05-11 15:19:28 +0600

    finalQubit

--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,8 @@ $(document).ready(function () {
         $('.vafli , .header__nav').toggleClass('active')
     });
 
+    $('#tel').inputmask('+\\9\\96 (999) 99 99 99');
+
     $(window).on('scroll', function () {
         if ($(this).scrollTop() > 500) {
             $('.go-top').fadeIn('slow')
@@ -27,10 +29,29 @@ $(document).ready(function () {
         }
     });
 
-    $('.header__btn, .hero__btn, .advantages__btn, .power__button-2, .demo__btn').on('click', function (e) {
+    $('.header__btn, .advantages__btn, .power__button-2, .demo__btn').on('click', function (e) {
         $('.overlay, .popup__book').fadeIn('1')
     });
+
+    $('.header__logIn').on('click', function (e){
+        $('.overlay, .popup__logIn').fadeIn('1')
+    });
+
+    $('.book__btn, .hero__btn').on('click', function (e){
+        e.preventDefault()
+        $('.overlay, .popup__thanks').fadeIn('1')
+        $('.popup__book').fadeOut('1')
+    });
+
+
+
     $('.popup__close').on('click', function () {
-        $('.overlay').fadeOut('slow')
+        $('.overlay, .popup__book, .popup__logIn, .popup__thanks').fadeOut('slow')
+    });
+
+    $('.overlay').on('click', function (e) {
+        if (e.target.className === 'overlay') {
+            $('.overlay, .popup__book, .popup__logIn, .popup__thanks').fadeOut('slow')
+        }
     });
 });
